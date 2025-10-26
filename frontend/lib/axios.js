ecd91b8d25d83b1410c8eb11cc83078331e9c337
musa sheriff <venom@kali.venom>
2025-10-26 02:27:30 +0100

    Fix axios configuration

--- frontend/lib/axios.js
+++ frontend/lib/axios.js
@@ -1,9 +1,8 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials: true // send cookies to the server
-}
-)
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+  withCredentials: true,
+});
 
 export default axiosInstance;
